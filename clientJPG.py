a6b15999d291be522024c1e989c3324d8a4f9767
davidlin <aunbdaunbd@gmail.com>
2018-07-08 13:10:00 +0800

    Add google speech. receive data from server.

--- clientJPG.py
+++ clientJPG.py
@@ -7,6 +7,7 @@ from PIL import Image # please pip3 install Pillow
 import struct
 import time
 from io import BytesIO
+import subprocess
 
 def NumpyToJPG(arr):
 	ret, buf = cv2.imencode( '.jpg', arr)
@@ -16,20 +17,35 @@ def NumpyToJPG(arr):
 def NumpyToPickle(arr):
 	return pickle.dumps(arr)
 
+# Open 2 camera.
 cap1 = cv2.VideoCapture(1)
 cap2 = cv2.VideoCapture(2)
+
+# Open socket.
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('192.168.43.137',8000))
+
 count = 0
 while True:
 	count += 1
+
+	# Get a frame in the captured video.
 	ret, frame1 = cap1.read()
 	ret, frame2 = cap2.read()
+	
 	data = NumpyToJPG(frame1)
 	if count == 10:
 		clientsocket.sendall(struct.pack("L", len(data))+data)
+		detect = clientsocket.recv(4096).decode().split(',')
+		if int(detect[0]) > 0:
+			process = subprocess.Popen(["python", "googleSpeech.py", detect[0], detect[1]])
+	
 	data = NumpyToJPG(frame2)
 	if count == 10:
 		clientsocket.sendall(struct.pack("L", len(data))+data)
+		detect = clientsocket.recv(4096).decode().split(',')
+		if int(detect[0]) > 0:
+			process = subprocess.Popen(["python", "googleSpeech.py", detect[0], detect[1]])
 		count = 0
+
 	print ("hi %d"%count)
